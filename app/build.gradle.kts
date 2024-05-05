4d8cdce34b353cd9c0fc893c0b464529d2284099
paridhimaheshwari <priti.paridhi@gmail.com>
2024-05-05 10:02:13 +0530

    working first:ui basics

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-android")
+}
+
+android {
+    namespace = "com.example.quizapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.quizapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+//    implementation("org.jetbrains.kotlin:kotlin-android-extensions")
+    implementation("androidx.appcompat:appcompat:1.3.0") // or the latest version
+    implementation("com.google.android.material:material:1.0.0")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.quizapp
+import android.widget.Button
+import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
+import android.content.Intent
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.quizapp.databinding.ActivityMainBinding
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+//        binding.btnStart.text = viewModel.btnStart
+        binding.btnStart.setOnClickListener {
+
+            if (binding.etName.text.toString().isEmpty()) {
+
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            // EN
+        }
+    }
+}
